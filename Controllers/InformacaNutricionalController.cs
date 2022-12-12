668451c34bcf8c9a16da72c84077abc89f37fade
Pedro Galdiano <pedrocastro014@gmail.com>
2022-12-11 21:24:53 -0300

    aaaa

--- Controllers/InformacaNutricionalController.cs
+++ Controllers/InformacaNutricionalController.cs
@@ -15,20 +15,34 @@ public class InformacaNutricionalController : ControllerBase
         _sqlDataAccess = sqlDataAccess;
     }
 
-    [HttpGet]
+    [HttpGet("ListarTodos")]
     public async Task<ActionResult<IEnumerable<NutrientesModel>>> ListarTodos()
     {
         var result = await _sqlDataAccess.ListarTodos();
         return Ok(result);
     }
 
-    [HttpPost]
+    [HttpPost("ListarPorIDs")]
     public async Task<ActionResult<IEnumerable<NutrientesModel>>> ListarPorIDs(int[] listaIDs)
     {
         bool anyZeroOrNegatives = listaIDs.Any(val => val < 1);
-        if (!anyZeroOrNegatives) return BadRequest();
+        if (anyZeroOrNegatives) return BadRequest();
 
         var result = await _sqlDataAccess.ListarPorIDs(listaIDs);
         return Ok(result);
     }
+
+    [HttpPost("Formula")]
+    public async Task<ActionResult<IEnumerable<NutrientesModel>>> Formula(Dictionary<int, int> formula)
+    {
+        if (formula.Keys.Any(val => val < 1)) return BadRequest();
+
+        if (formula.Values.Any(val => val < 1)) return BadRequest();
+
+        if (formula.Values.Sum() > 100) return BadRequest();
+
+
+        // var result = await _sqlDataAccess.ListarPorIDs(formula.Keys.ToArray());
+    }
+
 }
